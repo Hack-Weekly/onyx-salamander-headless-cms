85cd3f467e6056cc448538efabde52cf126506d1
ricardo-719 <teacherrickyricardo@gmail.com>
2023-06-08 15:52:01 +0700

    Update media settings url

--- headlessLlama/urls.py
+++ headlessLlama/urls.py
@@ -22,4 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^', include('cms.urls')),
-] + static(settings.MEDIA_URLS, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
