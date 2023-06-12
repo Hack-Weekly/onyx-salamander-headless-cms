9f2e28e8d2492dbce7c0b9ad9f5c69fcc8b23149
ricardo-719 <teacherrickyricardo@gmail.com>
2023-06-12 15:20:29 +0700

    Add blog-post model class

---
+++ src/models/blog-post.py
@@ -0,0 +1,34 @@
+"""models/blog-post.py
+
+This represents a blog post within the Onyx Salamander CMS.
+"""
+
+from typing import Optional, List
+from pydantic import BaseModel
+from datetime import datetime
+from models.base import Node
+
+class BlogPost(BaseModel):
+
+    Title: str
+    Author: str
+    Content: str
+    Published: Optional[bool] = False
+    Tags: Optional[List[str]] = None
+    #FeaturedImage: Optional[Image] = None
+    #Comments: Optional[List[Comment]] = None
+
+    #Metadata
+    Keywords: Optional[List[str]] = None
+
+    #User Metadata
+    Modifier: Optional[str] = None # Who last modified the blog post
+    Creator: Optional[str] = None # Who created the blog post
+    Owner: Optional[str] = None # Who owns the blog post
+
+    # Datetime Metadata
+    CreatedDate: Optional[datetime] = None
+    ModifiedDate: Optional[datetime] = None
+    PublishedDate: Optional[datetime] = None
+    ReviewDate: Optional[datetime] = None
+    ArchiveDate: Optional[datetime] = None
